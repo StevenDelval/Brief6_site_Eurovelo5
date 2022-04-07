daff921606669c415f56f82c1532cfbdafc42e84
StevenDsimplon <asteven59220@free.fr>
2022-04-07 12:33:06 +0200

    Ajout gpx + test map

--- js/api/cartel.js
+++ js/api/cartel.js
@@ -1,15 +1,16 @@
-const url = "http://20.229.152.219:1337";
+/* const url = "http://20.229.152.219:1337";
 const cartel = "/api/cartels/";
 const recupAll = "?populate=*";
-const section = document.querySelector("section.cartels");
+const section = document.querySelector("section.cartels"); */
 
 function printArticle(value) {
     for (let cartel of value.data) {
+
         article = document.createElement("article");
 
         //// Creation contenu gauche
         figure = document.createElement("figure");
-        
+
         imgetape = document.createElement("img");
         imgetape.src = url + cartel.attributes.imgetape.data.attributes.formats.small.url;
         imgetape.classList.add("imgEtape");
@@ -43,8 +44,8 @@ function printArticle(value) {
 
         ////// Niveau
         divNiv = document.createElement("div");
-        
-        
+
+
         circle = document.createElement("i");
         circle.classList.add("fa-solid");
         circle.classList.add("fa-circle");
@@ -88,22 +89,17 @@ function printArticle(value) {
 
         //Ajout de l'article a la section
         section.appendChild(article);
+        
     }
 }
 
-fetch(url + cartel + recupAll)
-    .then(function (res) {
-        if (res.ok) {
-            return res.json();
-        }
-    })
-    .then(function (value) {
-        console.log(value);
-        printArticle(value);
+/* fetch(url + cartel + recupAll)
+    .then(response => response.json())
+    .then(function (response) {
+        response.data.sort(function (a, b) {
+            return a.id - b.id;
+        })
 
-
+        printArticle(response);
     })
-    .catch(function (err) {
-        console.log("err")
-        // Une erreur est survenue
-    });
+    .catch(error => alert("Erreur :" + error)); */
--- js/itineraire.js
+++ js/itineraire.js
@@ -1,25 +1,74 @@
 var map = L.map('map').setView([50.62925,3.057256], 9);
-
 /// creation map
 var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
-///
-
-/* var marker = L.marker([50.9652183, 1.8630816]).addTo(map);*/
 
  // URL to your GPX file or the GPX itself
 //  Ajout Gpx //
-var gpx = "gpx/gpx_eurovelo5.gpx";
-
-new L.GPX(gpx, {async: true,
+var liengpx=["gpx/calais-ardres.gpx" ,
+"gpx/ardres-watten.gpx",
+"gpx/watten-st-omer.gpx",
+"gpx/st-omer-aire-sur-la-lys.gpx" ,
+"gpx/aire-sur-la-lys-st-venant.gpx",
+"gpx/st-venant-bethune.gpx" ,
+"gpx/bethunes-olhain.gpx" ,
+"gpx/olhain-angres.gpx" ,
+"gpx/angres-lens.gpx" ,
+"gpx/lens-don.gpx" ,
+"gpx/don-lille.gpx" ,
+"gpx/lille-wattrelos.gpx"];
+const url = "http://20.229.152.219:1337";
+const cartel = "/api/cartels/";
+const recupAll = "?populate=*";
+const section = document.querySelector("section.cartels");
+fetch(url + cartel + recupAll)
+    .then(response => response.json())
+    .then(function (response) {
+        response.data.sort(function (a, b) {
+            return a.id - b.id;
+        })
+        
+        printArticle(response);
+        for(let gpxetape of liengpx){
+new L.GPX(gpxetape, {
     polyline_options: {
     color: '#E5B9B5',
     weight: 7,
     lineCap: 'round'
   }
+}).on('mouseover',function (e){
+  this.setStyle({
+    color: 'yellow'
+  })
+}).on('mouseout',function (e){
+  this.setStyle({
+    color: '#E5B9B5'
+  })
 }).on('loaded', function(e) {
   map.fitBounds(e.target.getBounds());
-}).addTo(map);
+}).addTo(map);}
+    })
+    .catch(error => alert("Erreur :" + error));
+
+    
+for(let gpxetape of liengpx){
+new L.GPX(gpxetape, {
+    polyline_options: {
+    color: '#E5B9B5',
+    weight: 7,
+    lineCap: 'round'
+  }
+}).on('mouseover',function (e){
+  this.setStyle({
+    color: 'yellow'
+  })
+}).on('mouseout',function (e){
+  this.setStyle({
+    color: '#E5B9B5'
+  })
+}).on('loaded', function(e) {
+  map.fitBounds(e.target.getBounds());
+}).addTo(map);}
 //////////////////
