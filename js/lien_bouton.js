0591d77c080274c84521128b3a45156dfcc01032
StevenDsimplon <asteven59220@free.fr>
2022-04-15 13:49:53 +0200

    Correction erreur

---
+++ js/lien_bouton.js
@@ -0,0 +1,8 @@
+const cta = document.querySelector(".cta")
+cta.addEventListener('click',()=>{
+    document.location.href= "itineraire.html"
+})
+const ctaOmbre = document.querySelector(".cta-ombre")
+ctaOmbre.addEventListener('click',()=>{
+    document.location.href= "news.html"
+})
